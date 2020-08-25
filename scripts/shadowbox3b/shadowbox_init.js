eafab7556c0ea1ddc83ed01a4d0fc50b87cc3f43
saladusaladim <37935644+saladusaladim@users.noreply.github.com>
2020-08-25 16:14:35 +0200

    first commit

---
+++ scripts/shadowbox3b/shadowbox_init.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+Shadowbox.init({
+    // let's skip the automatic setup because we don't have any
+    // properly configured link elements on the page
+    skipSetup: true,
+	 players:    ["swf","flv","wmp","iframe","html","qt","img"]
+});
